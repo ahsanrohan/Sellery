629b62a03ec5afe23faf3b8f25aaa3ed5537dd96
jaberrio <jaberrio88@hotmail.com>
2021-01-30 23:07:26 -0500

    I did a lot of magic
    you wouldn't belive

---
+++ Assets/Twitter.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Web.Helpers;
+using Web.Twitter.API;
+using Web.Twitter.DataStructures;
+
+public class Twitter : MonoBehaviour
+{
+    public string TwitterApiConsumerKey;
+    public string TwitterApiConsumerSecret;
+    public Text tweet_text;
+
+    public Button a1, a2, a3, a4;
+
+    public WebAccessToken TwitterApiAccessToken;
+
+   
+    public string[] celebrities = { "kanyewest", "justinbieber", "taylorswift13", "jaden", "elonmusk", "JLo", "SnoopDogg", "Sethrogen", "icecube", "LilNasX", "KylieJenner", "chrissyteigen", "tyrabanks", "rihanna", "KimKardashian", "KrisJenner", "50cent", "MarthaStewart", "ladygaga", "iamcardib", "MoistCr1TiKaL", "ArianaGrande", "jaboukie", "souljaboy", "Drake", "tylerthecreator" };
+    public string[] politicians = { "JoeBiden", "berniesanders", "tedcruz", "BarackObama", "KamalaHarris", "GOPLeader", "mattgaetz", "GOPChairwoman", "HillaryClinton", "DonaldJTrumpJr", "MittRomney", "NYGovCuomo", "GovRonDeSantis", "AOC", "BillClinton", "SpeakerPelosi", "benshapiro", "TomiLahren" };
+
+    int answer;
+    int[] answers = { -1, -1, -1, -1 };
+    int tweet_num;
+    string nameAnswer;
+
+    int catigory = 0;
+
+    System.Random random;
+    // Start is called before the first frame update
+
+    [Header("GetLatestTweetsFromUserByScreenName")]
+    public Tweet[] tweets;
+
+    void Start()
+    {
+        TwitterApiAccessToken = WebHelper.GetTwitterApiAccessToken(TwitterApiConsumerKey, TwitterApiConsumerSecret);
+        random = new System.Random();
+        nextQuestion(getCatarray());
+
+
+    }
+
+    string[] getCatarray()
+    {
+        switch (catigory)
+        {
+            case 0:
+                return celebrities;
+            case 1:
+                return politicians;
+            default:
+                return null;
+        }
+    }
+    public void newTweets(String name)
+    {
+        tweets = await TwitterRestApiHelper.GetLatestTweetsFromUserByScreenName(name, this.TwitterApiAccessToken.access_token);
+    }
+
+    void nextQuestion(String[] names)
+    {
+        int j = random.Next(getCatarray().Length-1);
+        answer = j;
+        nameAnswer = getCatarray()[j];
+
+        answers[0] = j;
+
+        for (int i = 1; i < answers.Length; i++)
+        {
+            int temp = random.Next(getCatarray().Length-1);
+            for (int p = 0; p < answers.Length; p++)
+            {
+                while(temp == answers[p])
+                    temp = random.Next(getCatarray().Length - 1);
+            }
+            answers[i] = temp;
+        }
+
+        newTweets(getCatarray()[answer]);
+
+
+        
+        tweet_num = random.Next(tweets.Length - 1);
+    }
+    
+    public void checkButtonAnswer()
+    {
+        var go = EventSystem.current.currentSelectedGameObject;
+
+
+        if (go.GetComponentInChildren<Text>().text == nameAnswer)
+            nextQuestion(getCatarray());
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if(tweets != null)
+        {
+            tweet_text.text = tweets[tweet_num].text;
+
+            a1.GetComponentInChildren<Text>().text = getCatarray()[answers[0]];
+            a2.GetComponentInChildren<Text>().text = getCatarray()[answers[1]];
+            a3.GetComponentInChildren<Text>().text = getCatarray()[answers[2]];
+            a4.GetComponentInChildren<Text>().text = getCatarray()[answers[3]];
+        }
+    }
+}
